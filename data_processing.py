5bdf1ed1c59a61f669bc1cfd3566e8378edd1df1
swarna138 <swarna.learning@gmail.com>
2024-02-14 20:18:17 +0530

    Added a new function sum

--- data_processing.py
+++ data_processing.py
@@ -1,4 +1,12 @@
 
 print("adding all the data processing code related fraud detection")
-print ("test")
-print ("test")
+
+a = 5
+b = 7
+
+def sum(a,b):
+    c = a+b
+    return a
+
+print ("added a new function of sum")
+
